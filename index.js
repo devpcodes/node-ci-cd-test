a3dbf67da6f2091b97fa6b47553862f6cb293a61
waltersgit <designs.peter@gmail.com>
2019-04-15 12:21:26 +0800

    success test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
- res.send('hello world CI/CD-');
+ res.send('hello world CI/CD');
 });
 
 const server = app.listen(process.env.PORT || 7000,()=>{
