11c9f8a612c8761876e439ef35849f6109215e12
waltersgit <designs.peter@gmail.com>
2019-04-15 11:38:03 +0800

    ES5

--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
 var request = require('supertest');
 var {app, server} = require('../index.js');
 describe('GET /', function() {
-    it('respond with hello world', async function(done) {
+    it('respond with hello world', function(done) {
         // request(app).get('/').expect('hello world').end((err)=>{
         //     if(err) throw done(err);
         //     done();
         // });
-        // request(app).get('/').end((err, response)=>{
-        //     if(err) throw done(err);
-        //     expect(response.text).toEqual('hello world')
-        //     done();
-        // });
-        const result = await request(app).get("/");
-        expect(result.text).toEqual("hello world");
-        done();
+        request(app).get('/').end((err, response)=>{
+            if(err) throw done(err);
+            expect(response.text).toEqual('hello world')
+            done();
+        });
+        // const result = await request(app).get("/");
+        // expect(result.text).toEqual("hello world");
+        // done();
     });
     afterAll(()=>{
         server.close();
